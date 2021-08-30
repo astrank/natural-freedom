f77f111db172f073984543490e111dea7b56bceb
Martín Rabasa <martinrabasa@gmail.com>
2021-08-29 21:57:05 -0300

    update theme provider

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,11 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { sanityClient, PortableText } from "../lib/sanity";
 import { useTheme } from "../context/ThemeContext";
 import * as Switch from "@radix-ui/react-switch";
-import Image from "next/image";
 
-import sun from "../assets/sun.svg";
-import moon from "../assets/moon.svg";
+import MenuIcon from "../assets/MenuIcon";
+import SunIcon from "../assets/SunIcon";
+import MoonIcon from "../assets/MoonIcon";
 
 const postsQuery = `*[_type == "post"]{
   _id,
@@ -23,64 +23,44 @@ export default function Home({ posts }) {
   }
 
   return (
-    <div className="flex flex-row py-8 text-gray-700 bg-white transition duration-700 dark:bg-dark dark:text-gray-300 divide-x divide-gray-300">
-      <div className="w-96 flex-col gap-3 px-6 hidden lg:flex">
+    <div className="flex flex-col lg:flex-row lg:py-8 text-gray-700 bg-white dark:bg-dark dark:text-gray-300 transition duration-700 divide-x-none lg:divide-x lg:divide-gray-300">
+      <div className="w-full lg:w-96 flex flex-row lg:flex-col gap-3 py-8 lg:py-0 px-3 sm:px-6 md:px-14 lg:px-6 justify-between lg:justify-start">
         <div>
           <Switch.Root
             name="switch"
-            onCheckedChange={() => handleSwitch(name)}
-            className="switch bg-gray-700 shadow-md relative gap-3 flex flex-row rounded-full w-16 h-8 items-center justify-center"
+            checked={theme == "dark" ? true : false}
+            onCheckedChange={() => handleSwitch()}
+            className="switch bg-gray-700 shadow-md relative gap-3 flex flex-row rounded-full w-16 h-8 lg:mb-4 items-center justify-center"
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5 text-yellow-200"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
-              />
-            </svg>
+            <SunIcon />
             <Switch.Thumb className="thumb h-6 w-6 rounded-full bg-gray-200 z-20 absolute left-1" />
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5 text-white"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
-              />
-            </svg>
+            <MoonIcon />
           </Switch.Root>
         </div>
-        {posts &&
-          posts.map((post, index) => (
-            <div
-              key={post._id}
-              className="cursor-pointer text-md font-classic font-semibold hover:text-blue-400 hover:underline"
-              onClick={() => setCurrent(index)}
-            >
-              {post.title}
-            </div>
-          ))}
+        <div className="block lg:hidden cursor-pointer">
+          <MenuIcon />
+        </div>
+        <div className="hidden lg:flex lg:flex-col lg:gap-3">
+          {posts &&
+            posts.map((post, index) => (
+              <div
+                key={post._id}
+                className="cursor-pointer text-md font-classic font-semibold hover:text-blue-400 hover:underline"
+                onClick={() => setCurrent(index)}
+              >
+                {post.title}
+              </div>
+            ))}
+        </div>
       </div>
-      <div className="w-full px-6 md:px-14">
+      <div className="w-full px-3 sm:px-6 md:px-14">
         <div className="mb-6 flex flex-col gap-2">
           <h1 className="text-5xl font-bold">{posts[current]?.title}</h1>
-          <span className="text-lg text-blue-400 font-semibold">
+          <span className="text-lg font-semibold">
             Autor:
             <a
               href="https://www.youtube.com/c/ChironLast"
-              className="ml-2 hover:underline"
+              className="ml-2 text-blue-400 hover:underline"
               target="_blank"
             >
               {posts[current]?.author}

440fd71fa783dddbfff64307f982fc3b56feed57
Martín Rabasa <martinrabasa@gmail.com>
2021-09-02 02:02:20 -0300

    load theme first

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,17 @@
 import { ProvideTheme } from "../context/ThemeContext";
 import Head from "next/head";
+import { useEffect, useState } from 'react';
 import 'tailwindcss/tailwind.css';
 import '../styles/global.css';
 import { IdProvider } from "@radix-ui/react-id";
 
 function MyApp({ Component, pageProps }) {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, [])
+
   return (
     <>
       <IdProvider>
@@ -13,7 +20,9 @@ function MyApp({ Component, pageProps }) {
           <link rel="icon" href="/favicon.ico" />
         </Head>
         <ProvideTheme>
-          <Component {...pageProps} />
+          {isMounted &&
+            <Component {...pageProps} />
+          }
         </ProvideTheme>
       </IdProvider>
     </>
